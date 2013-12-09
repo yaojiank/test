55d59ad4c4407d57f93803755ff68b1a8aa5c01b
yaokuan.myk <yaokuan.myk@alibaba-inc.com>
2013-12-09 19:33:19 +0800

    dddd

--- first-mvn/src/main/java/com/yaojiank/app/App.java
+++ first-mvn/src/main/java/com/yaojiank/app/App.java
@@ -1,5 +1,6 @@
 package com.yaojiank.app;
 
+
 /**
  * Hello world!
  *
@@ -8,6 +9,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        int i = 0;
+++i;
     }
 }
