1e40af16f06fa3e7522825ddcb4a635733105a76
yaokuan.myk <yaokuan.myk@alibaba-inc.com>
2013-12-18 14:33:05 +0800

    .

--- first-mvn/src/main/java/com/yaojiank/app/A.java
+++
@@ -1,5 +0,0 @@
-package com.yaojiank.app;
-
-public class A {
-
-}
---
+++ first-mvn/src/main/java/com/yaojiank/app/a/A.java
@@ -0,0 +1,9 @@
+package com.yaojiank.app.a;
+
+public class A {
+	
+	public static void main(String[] args) {
+		System.out.println("hello a!");
+	}
+
+}
